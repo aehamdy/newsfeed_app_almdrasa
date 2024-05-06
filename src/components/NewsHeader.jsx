ef80988ce9689cc6d945144259d8dd5a22d960ec
aehamdy <ae.hamdy91@gmail.com>
2024-05-07 02:39:51 +0300

    build: added app components

---
+++ src/components/NewsHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+function NewsHeader() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">NewsFeed App</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default NewsHeader;
